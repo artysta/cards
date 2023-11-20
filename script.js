1957fc4db40deb244658106e0cd93f5e030b74a2
Adrian Kurek <adrian.kurek93@gmail.com>
2023-11-20 18:12:59 +0100

    Use svg flag instead of emoji

--- script.js
+++ script.js
@@ -18,12 +18,15 @@ async function renderCards() {
     words.forEach(w => {
         let index = 0;
         const card = document.createElement('div');
-        const flag = document.createElement('p');
+        // const flag = document.createElement('p');
+        const flag = document.createElement('img');
         const word = document.createElement('p');
         const sentence = document.createElement('p');
         
         card.classList += 'card';
-        flag.innerHTML = languages[index].flag;
+        // flag.innerHTML = languages[index].emojiFlag;
+        flag.src = languages[index].svgFlagPath;
+        flag.classList += 'flag';
         word.innerHTML = w.plWord;
         sentence.innerHTML = w.plSentence;
         card.appendChild(flag);
@@ -33,7 +36,8 @@ async function renderCards() {
 
         card.onclick = () => {
             index = index >= 2 ? 0 : index + 1;
-            flag.innerHTML = languages[index].flag;
+            // flag.innerHTML = languages[index].emojiFlag;
+            flag.src = languages[index].svgFlagPath;
             word.innerHTML = w[languages[index].word];
             sentence.innerHTML = w[languages[index].sentence];
             card.style.backgroundColor = languages[index].color;
